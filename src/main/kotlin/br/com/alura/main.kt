992fed854f2075b60c9c757e09e5b3580cb53aee
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-11 19:03:41 -0300

    04.01 Leitura e filtros específicos de Map

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -1,22 +1,52 @@
 package br.com.alura
 
-import br.com.alura.test.testaSet
-
 fun main() {
     val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(
         Pair(1, 20.0),
-        Pair(2, 34.0),
-        3 to 50.0
+        Pair(2, 64.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 150.0,
+        6 to 80.0
     )
+
+    /*val valorPedido: Double? = pedidos.getValue(5)
+    println(valorPedido)*/
+
+    val mensagem = pedidos.getOrElse(1, { "Não tem o pedido" })
+    println(mensagem)
+
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+
+    for(numerosdePedido in pedidos.keys) {
+        print("Pedido $numerosdePedido ")
+    }
+    println()
+    for(valorDoPedido in pedidos.values) {
+        print("Valor $valorDoPedido ")
+    }
+
+    pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 50}
+
+    println(pedidos.values)
+
+
+
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     println(pedidos)
 
     val pedido: Double? = pedidos[4]
 
-    pedido?.let{
+    pedido?.let {
         println("pedido $it")
     }
 
-    for (p: Map.Entry<Int, Double> in pedidos){
+    for (p: Map.Entry<Int, Double> in pedidos) {
         println("Numero do pedido: ${p.key}")
         println("Valor do pedido: ${p.value}")
     }
@@ -40,9 +70,6 @@ fun main() {
     println(pedidos)
     pedidos.remove(3, 50.0)
     println(pedidos)
-
-
-
 }
 
 
